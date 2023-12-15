1a42ee3cbca765f01379faec4ecc0ea2b2df050f
Aistis Vyšniauskas <devpastas@gmail.com>
2023-12-15 15:41:58 +0200

    corrected data models

--- src/models/answer.js
+++ src/models/answer.js
@@ -1,1 +1,7 @@
 import mongoose from "mongoose";
+
+const answerSchema = mongoose.Schema({
+    "content": {type: String, required: true}
+})
+
+export default mongoose.model("Answer", answerSchema);
--- src/models/question.js
+++ src/models/question.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
 
 const questionSchema = mongoose.Schema({
-    title: {type: String, required: true},
-    description: {type: String, required: true}
+    "title": {type: String, required: true},
+    "description": {type: String, required: true}
 })
 
 
-export default mongoose.model(questionSchema, "questionSchema");
+export default mongoose.model("Question", questionSchema);
